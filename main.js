82a0e7abb612e0cfea011761f8a280f0f68006ac
Andrey Sarapionov <andrey1@pacto.io>
2017-01-12 13:20:01 +0200

    Global Objects

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-// Let's try to print the value of __filename
-
-console.log( __filename );
+function printHello(){
+   console.log( "Hello, World!");
+}
+// Now call above function after 2 seconds
+setInterval(printHello, 2000);
