8a7be37327be90a2ee1853684383ec548135fe6b
Andrey Sarapionov <andrey1@pacto.io>
2017-01-13 14:54:56 +0200

    nodejs_restful_api and eror Cannot GET /addUser

--- server.js
+++ server.js
@@ -2,46 +2,29 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
-var bodyParser = require('body-parser');
-var multer = require('multer');
-var upload = multer({ dest: '/tmp/'});
+var user = {
+   "user4" : {
+      "name" : "mohit",
+      "password" : "password4",
+      "profession" : "teacher",
+      "id": 4
+   }
+}
 
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.get('/index.htm', function (req, res) {
-   res.sendFile( __dirname + "/" + "index.htm" );
-});
-
-app.post('/file_upload', upload.single('file'), function (req, res) {
-   console.log(req.file.name);
-   console.log(req.file.path);
-   console.log(req.file.type);
-
-   var file = __dirname + "/" + req.file.originalname;
-
-   console.log("writing file called: ", file);
-   
-   fs.readFile( req.file.path, function (err, data) {
-      fs.writeFile(file, data, function (err) {
-         if( err ){
-            console.log("error", err );
-            } else {
-               response = {
-                  message:'File uploaded successfully',
-                  filename: file
-               };
-            }
-         console.log( response );
-         res.end( JSON.stringify( response ) );
-      });
-
+app.post('/addUser', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       data = JSON.parse( data );
+       data["user4"] = user["user4"];
+       console.log( data );
+       res.end( JSON.stringify(data));
    });
 })
 
 var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
+
+  var host = server.address().address
+  var port = server.address().port
+  console.log("Example app listening at http://%s:%s", host, port)
+
 })
